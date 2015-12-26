db6eb4dee7b3bf64d188b14a022572bf7da51148
Stan <iamjacke@gmail.com>
2015-12-26 23:10:51 +0600

    DBOARD Updates [skip ci] [ci skip]

--- databoard/play-silhouette-seed-master/app/controllers/BoardController.scala
+++ databoard/play-silhouette-seed-master/app/controllers/BoardController.scala
@@ -85,9 +85,81 @@ class BoardController @Inject() (val reactiveMongoApi: ReactiveMongoApi,val mess
       Logger.info(s"Checked index, result is $index")
   }
 
-/*******
+ def toInt(s: String): Option[Int] = {
+  try {
+    Some(s.toInt)
+  } catch {
+    case e: Exception => None
+  }
+}
+ def parseParam(stringAsInt: Option[String]):Option[Int] = {
+  stringAsInt match {
+    case Some(string) => {
+      toInt(string)
+    }
+    case _ => None
+  }
+ } 
+/******
  * list all boards and sort them
  *******/
+
+def findOrCreate(launch_idOpt: Option[String], element_idOpt: Option[String], userIdOpt: Option[String]) = Action.async { implicit request =>
+    val query = BSONDocument(
+      "query" -> BSONDocument())
+
+    // FIND
+    val launch_id = parseParam(launch_idOpt)
+    val element_id = parseParam(element_idOpt)
+    val userId = userIdOpt.getOrElse("")
+    var metas = BSONArray.empty
+          launch_id match {
+            case Some(launch_id) => element_id match {
+              case Some(element_id) => metas = metas ++ BSONDocument("launch_id" -> launch_id.toString, 
+                                                                     "element_id" -> element_id.toString) 
+              case _ => metas = metas ++ BSONDocument("launch_id" -> launch_id.toString)
+
+            }
+            case _ => 
+          }
+    val metasObj = launch_id match {
+      case Some(launch_id) => element_id match {
+        case Some(element_id) => List(MetaVal(key = "element_id", value = element_id.toString), MetaVal(key = "launch_id", value = launch_id.toString))
+        case _ => List(MetaVal(key = "launch_id", value = launch_id.toString))
+      }
+      case _ => List()
+    }        
+
+    val cursor = collection.find(Json.obj("query" -> 
+      BSONDocument("metas" -> metas))).cursor[Board](readPreference = ReadPreference.primary)
+    //val user = Some(request.identity)
+     val future = cursor.collect[List]()
+    // OR CREATE 
+    cursor.collect[List]().flatMap { board =>
+               val searchedBoard = board.headOption
+               searchedBoard match {
+                case Some(board) => Future(Ok(Json.toJson(board)))
+                case _ => { 
+                  val boardId = UUID.randomUUID()
+                  val board = Board(
+                                id = Some(boardId),
+                              title = boardId.toString,
+                              content = "",
+                              publisher = userId,
+                              ownership = Ownership(host = "min.ority.us", uid = userId),
+                              meta = metasObj, None,None)
+                          collection.insert(board.copy(creationDate = Some(new DateTime()), 
+                                               updateDate = Some(new DateTime()))).map(_ =>
+                     Ok(Json.toJson(board)))
+                } // Create board
+               }
+
+
+    }
+
+    //Future(Ok(Json.toJson("result")))
+}
+
   def index = Action.async { implicit request =>
     // get a sort document (see getSort method for more information)  
     val sort = getSort(request)
